6a40b23c337b98dd594291f2061a67237c462c2a
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-10-22 11:38:14 +0200

    add AddNumbers

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Example
+{
+    public class Calculator
+    {
+        // Adds two integer numbers and returns their sum
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result1 = calc.AddNumbers(5, 3);
+
+            Console.WriteLine($"AddNumbers result: {result1}");
+        }
+    }
+}
